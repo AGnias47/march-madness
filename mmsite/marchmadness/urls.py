f4699621a189db2394bd75d778ec95d2922e762e
Andy Gnias <AGnias47@gmail.com>
2024-08-20 09:53:25 -0400

    Add schools

--- mmsite/marchmadness/urls.py
+++ mmsite/marchmadness/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("school/<str:school_name>", views.school, name="school"),
+]
--- mmsite/marchmadness/views.py
+++ mmsite/marchmadness/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from .models import School
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the March Madness index.")
+
+
+def school(request, school_name):
+    school_name = school_name.title()
+    school = School.objects.filter(name=school_name).first()
+    return HttpResponse(school)
