2c67ccd6a8bddd324978f0ea2bdba09c9789fd12
Andy Gnias <AGnias47@gmail.com>
2024-08-20 09:42:02 -0400

    Update admin

--- mmsite/marchmadness/admin.py
+++ mmsite/marchmadness/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import School
+from .models import APRanking, Game, Rivalry, School, Tournament, TournamentRanking
 
+admin.site.register(APRanking)
+admin.site.register(Game)
+admin.site.register(Rivalry)
 admin.site.register(School)
+admin.site.register(Tournament)
+admin.site.register(TournamentRanking)
