36db3229dab5e6d4df05f9d36363982065483572
Andy Gnias <AGnias47@gmail.com>
2024-08-30 13:35:58 -0400

    Refactor helpers

--- mmsite/marchmadness/helpers/teams.py
+++ mmsite/marchmadness/helpers/teams.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import MultipleObjectsReturned
 
-from .models.school import School
+from ..models.school import School
 
 
 def get_teams_from_rankings(group, team_1_ranking, team_2_ranking):
